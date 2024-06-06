3e4ac233ec1927206e4d7f36292932eea99f7600
Kamit6337 <kamit6337@gmail.com>
2024-06-06 09:54:29 +0530

    update setCookies

--- utils/auth/setCookies.js
+++ utils/auth/setCookies.js
@@ -4,6 +4,8 @@ import { cookies } from "next/headers";
 const setCookies = (obj) => {
   const createToken = encrypt(obj);
 
+  console.log("createToken", createToken);
+
   cookies().set("tok", createToken, {
     httpOnly: true,
     maxAge: 7 * 24 * 60 * 60 * 1000,
